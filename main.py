cfb065bf5083cb81f4e1507b21af05c0bcdc9172
thodotpro <tho.pro@posteo.org>
2025-03-07 17:54:38 +0100

    Basic warehouse functionality added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 class Shop(ABC):
     pass
 
+### Product Interface ###
 class Products(Shop):
     """To initialize any product you're required to confirm: A product number, a product name, a price and the quantity.
     """
@@ -12,35 +13,35 @@ class Products(Shop):
         self.product_price = product_price
         self.product_quantity = product_quantity
 
+    def __repr__(self):
+        return f"{self.product_number} - {self.product_name}: {self.product_price} - {self.product_quantity}"
+
+
+### Warehouse Interface ###
+class Warehouse(Shop):
     @abstractmethod
-    def add_to_warehouse():
-        """Adds product to the warehouse.
-        """
+    def add_to_warehouse(Product):
         pass
 
-    def rem_from_warehouse():
+    @abstractmethod
+    def rem_from_warehouse(Product):
         """Removes a product from the warehouse.
         """
         pass
 
-class Warehouse(Shop):
     @abstractmethod
-    def create_warehouse()->dict:
-        pass
-
-    @abstractmethod
-    def get_warehouse():
+    def get_warehouse(Warehouse):
         """"Returns a dictionairy of all products in the warehouse.
         """
         pass
 
     @abstractmethod
-    def check_availabilty()->bool:
+    def check_availabilty(Warehouse, Product)->bool:
         """Returns True if this product is available.
         """
         pass
 
-
+### Shopping Cart Interface ###
 class ShoppingCart(Shop):
     @abstractmethod
     def create_cart()->list:
@@ -59,7 +60,7 @@ class ShoppingCart(Shop):
         """Removes items from shopping cart.
         """
         pass
-
+### Payment Interface ###
 class Payment(Shop):
     @abstractmethod
     def confirm_payment()->bool:
@@ -67,20 +68,75 @@ class Payment(Shop):
         """
         pass
 
+### Simple Storage ###
+class SimpleStorage(Warehouse):
+    def __init__(self, location: str, size: float):
+        self.location = location
+        self.size = size
+        self.inventory = {}
+
+    def __repr__(self):
+        return f"Location: {self.location}, Size: {self.size}"
+
+    def add_to_warehouse(self, Product):
+        """Adds a product to the warehouse.
+        """
+        self.inventory.update({Product.product_number: [Product.product_name, Product.product_price, Product.product_quantity]})
+        return self.inventory
+
+    def rem_from_warehouse(self, Product):
+        """Removes a product from the warehouse.
+        """
+        self.inventory.pop(Product.product_number)
+
+    def get_warehouse(self):
+        """Returns all available items in the warehouse.
+        """
+        for k in self.inventory:
+            print(f"{k}: {self.inventory[k]}")
+
+
+    def check_availabilty(self, Product):
+        """Returns True if this product is available.
+        """
+        if Product.product_number in self.inventory:
+            print(f"{Product.product_number} - {Product.product_name} is available. {Product.product_quantity} in stock.")
+            return True
+        else:
+            print("Product is not available")
+            return False
+
 class Article(Products):
     def __init__(self, product_number, product_name, product_price, product_quantity, edible: bool):
         super().__init__(product_number, product_name, product_price, product_quantity)
         self.edible = edible
 
-    def add_to_warehouse(self, warehouse):
-        raise NotImplementedError
+    def need_cooling(self)->bool:
+        if self.edible == True:
+            return True
+        else:
+            return False
 
-    def is_edible(self)->bool:
-        pass
+
 
 
 
 if __name__ == "__main__":
     shop = Shop()
-    shoe = Article("0001", "Gummies", 99.9, 1000, True)
+    shoe = Article(product_number="0001", product_name="Gummies", product_price=0.99, product_quantity=1000, edible=True)
+    shirt = Article(product_number="0002", product_name="T-Shirt", product_price=19.99, product_quantity=100, edible=False)
+
+
+    store = SimpleStorage("Strasse 123", 1000)
+
+    store.add_to_warehouse(shoe)
+    store.add_to_warehouse(shirt)
+    print(store.inventory)
+
+    # store.rem_from_warehouse(shoe)
+    store.get_warehouse()
+
+    store.check_availabilty(shoe)
+    store.check_availabilty(shirt)
+
 
